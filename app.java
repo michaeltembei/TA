4c23eadbdbc5266d1eb7c1ad4772b0f31236f737
root <root@ip-172-31-27-210.us-west-1.compute.internal>
2022-11-25 17:03:54 +0000

    first commit

---
+++ app.java
@@ -0,0 +1,7 @@
+# where are you from
+# who is the current US president
+# What is congress
+# What is senate
+# what is the largest US state
+# what is the most polulated US state
+# author: Michael A
